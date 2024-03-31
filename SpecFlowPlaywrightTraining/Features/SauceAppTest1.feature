4b637b92f56910deb6777d2ec1c0ebb65d046d95
jamie <jamie.l.hart07@gmail.com>
2024-03-31 12:43:45 +0100

    Add hooks and running tests in Parallel

---
+++ SpecFlowPlaywrightTraining/Features/SauceAppTest1.feature
@@ -0,0 +1,9 @@
+﻿Feature: SauceAppTest1
+
+@Smoke
+Scenario: Test Login operation of Sauce Application1
+	Given I navigate to the application
+	And I enter the following login details
+		| UserName      | Password     |
+		| standard_user | secret_sauce |
+	Then I see the Products list
---
+++ SpecFlowPlaywrightTraining/Hooks/Hook.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+[assembly:Parallelizable(ParallelScope.Fixtures)]
+
+
+namespace SpecFlowPlaywrightTraining.Hooks
+{
+    [Binding]
+    internal class Hooks
+    {
+    }
+}
